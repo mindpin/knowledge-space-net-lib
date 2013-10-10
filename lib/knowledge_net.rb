3972568155a4d6153a7416d0caaaa043745d3314
menxu <menxu_work@163.com>
2013-10-10 17:33:37 +0800

    update array updated Hash

--- lib/knowledge_net.rb
+++ lib/knowledge_net.rb
@@ -4,24 +4,23 @@ class KnowledgeNet
   attr_accessor :knowledge_nodes
 
   def initialize(doc)
-    kns = []
+    hash = {}
     doc.css("nodes node").each do |node|
-      kns << KnowledgeNode.new(
-          :id => node.attr("id"),
-          :name => node.css("name").first.text,
-          :desc => node.css("desc").first.text
-        )
+      id = node.attr("id")
+      hash[id] = KnowledgeNode.new(
+                                    :id => id,
+                                    :name => node.css("name").first.text,
+                                    :desc => node.css("desc").first.text
+                                  )
     end
 
     doc.css("relations relation").map do |relation|
-      parent_arr =  get_css_arr(relation.css("parent"))
-      child_arr  =  get_css_arr(relation.css("child"))
-      kns.map do |knowledge_node|
-        knowledge_node.children = knowledge_node.children + child_arr if parent_arr.include?(knowledge_node.id)
-        knowledge_node.parents = knowledge_node.parents + parent_arr  if child_arr.include?(knowledge_node.id)
-      end
+      parent_id =  relation.css("parent").first.attr("node_id")
+      child_id  =  relation.css("child").first.attr("node_id")
+      hash[parent_id].children << child_id if hash[parent_id] != nil
+      hash[child_id].parents << parent_id if hash[child_id] != nil 
     end
-    self.knowledge_nodes = kns
+    self.knowledge_nodes = hash
   end
 
   def self.load_xml_file(file_path)
@@ -31,25 +30,13 @@ class KnowledgeNet
 
   def root_nodes
     kns = []
-    self.knowledge_nodes.each do |node|
-      kns << node if node.is_no_parents
+    self.knowledge_nodes.each do |k,v|
+      kns << v if v.is_no_parents
     end
     kns
   end
 
   def find_node_by_id(node_id)
-    node = self.knowledge_nodes.each do |node|
-      return node if node.id == node_id
-    end
-    node
+    self.knowledge_nodes[node_id] || nil
   end
-
-  private
-    def get_css_arr(css_arr)
-      arr=[]
-      css_arr.each do |css|
-        arr << css.attr("node_id")
-      end
-      arr
-    end
 end
