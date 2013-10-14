9c407fe7a6e8f6a9f908e976fcc7dd5077437f5e
fushang318 <fushang318@gmail.com>
2013-10-14 15:35:53 +0800

    fix

--- lib/knowledge-space-net-lib.rb
+++ lib/knowledge-space-net-lib.rb
@@ -7,7 +7,11 @@ require 'knowledge_space_relation'
 
 module KnowledgeSpaceNetLib
   if defined?(Rails)
-    BASE_PATH = Rails.root.to_s
+    class Railtie < Rails::Railtie
+      initializer "init_base_path" do
+        KnowledgeSpaceNetLib::BASE_PATH = Rails.root.to_s
+      end
+    end
   else
     BASE_PATH = File.expand_path("../../",__FILE__)
   end
